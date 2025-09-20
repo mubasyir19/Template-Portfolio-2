1d3ffa8d9426a58cd8b6ed78a1af7f6c07c10c67
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-09-20 23:37:50 +0700

    update: responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,24 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className=''>
-      <section className='h-screen flex items-center justify-center'>
+      <section className='h-screen px-5 flex items-center justify-center'>
         <div className=''>
-          <div className='mx-auto bg-linear-to-br from-[#FF8660] to-[#9A33FF] rounded-full size-52 overflow-hidden'>
-            <Image src={`/images/avatar2.png`} width={213} height={213} alt='avatar' className='size-60 object-cover' />
+          <div className='mx-auto bg-linear-to-br from-[#FF8660] to-[#9A33FF] rounded-full size-36 lg:size-52 overflow-hidden'>
+            <Image
+              src={`/images/avatar2.png`}
+              width={213}
+              height={213}
+              alt='avatar'
+              className='size-40 lg:size-60 object-cover'
+            />
           </div>
-          <h1 className='mt-10 text-white text-center font-extrabold text-6xl'>
+          <h1 className='mt-10 text-white text-center font-extrabold text-3xl lg:text-6xl'>
             I do code and <br /> make content{' '}
             <span className='bg-linear-to-br from-[#FF8660]  to-[#9A33FF] bg-clip-text text-transparent'>
               about it!
             </span>
           </h1>
-          <p className='mt-10 text-[#C5C5C5] text-center w-1/2 mx-auto'>
+          <p className='mt-10 text-[#C5C5C5] text-center w-full lg:w-1/2 mx-auto'>
             I am a seasoned full-stack software engineer with over 8 years of professional experience, specializing in
             backend development. My expertise lies in crafting robust and scalable SaaS-based architectures on the
             Amazon AWS platform.
@@ -30,17 +36,17 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='py-20'>
+      <section className='py-20 px-5'>
         <h3 className='text-[#C5C5C5] font-semibold uppercase text-center text-xl'>Experience With</h3>
-        <div className='flex items-center justify-center gap-14 mt-12'>
+        <div className='flex flex-wrap items-center justify-center gap-14 mt-12'>
           {skills.map((sk, index) => (
             <Image key={index} src={sk.link} width={42} height={42} alt={sk.name} className='h-12 w-auto' />
           ))}
         </div>
       </section>
-      <section className='py-12'>
+      <section className='py-12 px-5'>
         <h2 className='text-4xl font-extrabold text-center text-[#FF8660]'>Projects</h2>
-        <div className='mt-7 grid grid-cols-1 lg:grid-cols-2 w-3/5 mx-auto gap-7 justify-items-center'>
+        <div className='mt-7 grid grid-cols-1 md:grid-cols-2 w-full lg:w-3/5 mx-auto gap-7 justify-items-center'>
           <div id='project' className='w-fit'>
             <Image
               src={`/images/projects/project1.png`}
@@ -83,11 +89,11 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='py-12'>
+      <section className='py-12 px-5'>
         <h2 className='text-4xl font-extrabold text-center bg-linear-to-r from-[#5BFF9F] to-[#13D195] bg-clip-text text-transparent'>
           Education
         </h2>
-        <div className='mt-14 w-3/5 mx-auto space-y-12'>
+        <div className='mt-14 w-full lg:w-3/5 mx-auto space-y-12'>
           <div id='experience' className=''>
             <div className='flex items-center justify-between'>
               <div className='flex items-center gap-3.5'>
@@ -112,11 +118,11 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='py-12'>
+      <section className='py-12 px-5'>
         <h2 className='text-4xl font-extrabold text-center bg-linear-to-r from-[#5BADFF] to-[#1373D1] bg-clip-text text-transparent'>
           Experience
         </h2>
-        <div id='list-experience' className='mt-14 w-3/5 mx-auto space-y-12'>
+        <div id='list-experience' className='mt-14 w-full lg:w-3/5 mx-auto space-y-12'>
           <div id='experience' className=''>
             <div className='flex items-center justify-between'>
               <div className='flex items-center gap-2.5'>
@@ -175,10 +181,10 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <footer className='py-24 bg-[#191919]'>
-        <div className='w-3/5 mx-auto'>
+      <footer className='py-24 px-5 bg-[#191919]'>
+        <div className='w-full lg:w-3/5 mx-auto'>
           <h3 className='text-white font-bold text-2xl'>Contact</h3>
-          <p className='mt-9 w-3/4 text-light-grey text-sm'>
+          <p className='mt-9 w-full lg:w-3/4 text-light-grey text-sm'>
             Seasoned Full Stack Software Engineer with over 8 years of hands-on experience in designing and implementing
             robust, scalable, and innovative web solutions. Adept at leveraging a comprehensive skill set encompassing
             front-end and back-end technologies{' '}
